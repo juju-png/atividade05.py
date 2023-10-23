206d40e7f640f2e95bf3ee1e760d5c0225d2cefc
juju-png <juliocesar71156@gmail.com>
2023-10-23 13:49:42 -0300

    atividade 05

---
+++ atividade05.py
@@ -0,0 +1,11 @@
+# Digite uma metragem e veja isso em cm e mm:
+metragem = ("digite uma metragem")
+cm = centimetro = 100 
+mm = metros = 1000 
+
+print(f"""
+      Seu numero é: {metros} m 
+      Conversão em MM: {mm} mm 
+      Conversão em CM: {cm}  cm
+      
+      """)
